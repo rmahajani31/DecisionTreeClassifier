cfaab5c3e38f97b0997dc0260874b333a77bf93b
Rishabh Mahajani <rishabh.mahajani@hilabs.com>
2020-02-08 18:11:30 -0500

    Created and tested an encoder for categorical variables

--- test_DecisionTreeClassifier/TestDecisionTreeClassifier.py
+++ test_DecisionTreeClassifier/TestDecisionTreeClassifier.py
@@ -1,13 +1,15 @@
 import unittest
 from DecisionTreeClassifier import *
-
+from Encoder import *
 
 class TestDecisionTreeClassifier(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.test_file = "test_decision_tree.csv"
+        cls.test_file = "test_encoder.csv"
         cls.dt_classifier = DecisionTreeClassifier(cls.test_file, "info gain", 1, 2)
+        cls.en = Encoder(cls.dt_classifier.data, ["Gender", "Color", "Sport", "Dominant Hand", "Home State", "Allergy", "Food"])
+        cls.dt_classifier.data = cls.en.encode()
 
     def test_calculate_entropy(self):
         data = self.dt_classifier.data
@@ -21,61 +23,61 @@ class TestDecisionTreeClassifier(unittest.TestCase):
         data = self.dt_classifier.data
         self.assertEqual(self.dt_classifier.get_majority_class(data), "B")
 
-    def test_calculate_split_entropy_f1(self):
+    def test_calculate_split_entropy_Gender(self):
         data = self.dt_classifier.data
-        left_data = data.loc[data["f1"] == 0, :]
-        right_data = data.loc[data["f1"] == 1, :]
+        left_data = data.loc[data["Gender"] == 0, :]
+        right_data = data.loc[data["Gender"] == 1, :]
         self.assertEqual(self.dt_classifier.calculate_split_entropy(self.dt_classifier.data, left_data, right_data), 1)
 
-    def test_calculate_split_entropy_f2(self):
+    def test_calculate_split_entropy_Color(self):
         data = self.dt_classifier.data
-        left_data = data.loc[data["f2"] == 0, :]
-        right_data = data.loc[data["f2"] == 1, :]
+        left_data = data.loc[data["Color"] == 0, :]
+        right_data = data.loc[data["Color"] == 1, :]
         self.assertEqual(self.dt_classifier.calculate_split_entropy(self.dt_classifier.data, left_data, right_data), -1 * (0.7 * math.log2(0.7) + 0.3 * math.log2(0.3)))
 
-    def test_calculate_split_entropy_f3(self):
+    def test_calculate_split_entropy_Sport(self):
         data = self.dt_classifier.data
-        left_data = data.loc[data["f3"] == 0, :]
-        right_data = data.loc[data["f3"] == 1, :]
+        left_data = data.loc[data["Sport"] == 0, :]
+        right_data = data.loc[data["Sport"] == 1, :]
         self.assertEqual(self.dt_classifier.calculate_split_entropy(self.dt_classifier.data, left_data, right_data),
                          -1 * (0.6 * math.log2(0.6) + 0.4 * math.log2(0.4)))
 
-    def test_calculate_split_gini_index_f1(self):
+    def test_calculate_split_gini_index_Gender(self):
         data = self.dt_classifier.data
-        left_data = data.loc[data["f1"] == 0, :]
-        right_data = data.loc[data["f1"] == 1, :]
+        left_data = data.loc[data["Gender"] == 0, :]
+        right_data = data.loc[data["Gender"] == 1, :]
         self.assertEqual(self.dt_classifier.calculate_split_gini_index(self.dt_classifier.data, left_data, right_data),
                          1 - (0.5**2 + 0.5**2))
 
     def test_calculate_split_gini_index_f2(self):
         data = self.dt_classifier.data
-        left_data = data.loc[data["f2"] == 0, :]
-        right_data = data.loc[data["f2"] == 1, :]
+        left_data = data.loc[data["Color"] == 0, :]
+        right_data = data.loc[data["Color"] == 1, :]
         self.assertEqual(self.dt_classifier.calculate_split_gini_index(self.dt_classifier.data, left_data, right_data),
                          1 - (0.7**2 + 0.3**2))
 
     def test_calculate_split_gini_index_f3(self):
         data = self.dt_classifier.data
-        left_data = data.loc[data["f3"] == 0, :]
-        right_data = data.loc[data["f3"] == 1, :]
+        left_data = data.loc[data["Sport"] == 0, :]
+        right_data = data.loc[data["Sport"] == 1, :]
         self.assertEqual(self.dt_classifier.calculate_split_gini_index(self.dt_classifier.data, left_data, right_data),
                          1 - (0.6**2 + 0.4**2))
 
     def test_calculate_info_gain_f1(self):
         data = self.dt_classifier.data
-        self.assertAlmostEqual(self.dt_classifier.calculate_info_gain(self.dt_classifier.calculate_entropy(data), data, "f1"), 0.1245112498)
+        self.assertAlmostEqual(self.dt_classifier.calculate_info_gain(self.dt_classifier.calculate_entropy(data), data, "Gender"), 0.1245112498)
 
     def test_calculate_info_gain_f5(self):
         data = self.dt_classifier.data
-        self.assertAlmostEqual(self.dt_classifier.calculate_info_gain(self.dt_classifier.calculate_entropy(data), data, "f5"), 0.1735337494)
+        self.assertAlmostEqual(self.dt_classifier.calculate_info_gain(self.dt_classifier.calculate_entropy(data), data, "Home State"), 0.1735337494)
 
     def test_calculate_gini_gain_f2(self):
         data = self.dt_classifier.data
-        self.assertAlmostEqual(self.dt_classifier.calculate_gini_gain(self.dt_classifier.calculate_gini_index(data), data, "f2"), 0.0542857143)
+        self.assertAlmostEqual(self.dt_classifier.calculate_gini_gain(self.dt_classifier.calculate_gini_index(data), data, "Color"), 0.0542857143)
 
     def test_calculate_gini_gain_f7(self):
         data = self.dt_classifier.data
-        self.assertAlmostEqual(self.dt_classifier.calculate_gini_gain(self.dt_classifier.calculate_gini_index(data), data, "f7"), 0.123333333333)
+        self.assertAlmostEqual(self.dt_classifier.calculate_gini_gain(self.dt_classifier.calculate_gini_index(data), data, "Food"), 0.123333333333)
 
     def test_train(self):
         self.dt_classifier.train()
